34ce8d5cc41c442c3949530fb09de4abc7d53ab6
tfcvbj6777 <82308262+tfcvbj6777@users.noreply.github.com>
2021-04-25 17:33:44 +0600

    Update and rename .gitignore to id.py

---
+++ id.py
@@ -0,0 +1,11 @@
+
+import amino 
+client=amino.Client()
+client.login(email=email, password=password)
+while True:
+    link = input('ану быстра ввел че те нада узнать: ');
+    info = client.get_from_code(link)
+    info = info.json["extensions"]["linkInfo"];
+    comid  = info["ndcId"];
+    objectid = info["objectId"];
+    print(f'ID сообщества: {comid} ID объекта: {objectid}')
